8aeab65664ff564b80677ba88ae9c637225b3be1
palantirstopguy <kocmateos@gmail.com>
2026-03-31 14:46:37 +0200

    created a function to get the model

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
+def get_model(num_classes):
+    model = models.efficientnet_b0(weights="IMAGENET1K_V1")
 
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+
+    return model
